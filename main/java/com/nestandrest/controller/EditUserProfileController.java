280641c8b28b4d23c023b42e6dd44139449cad3e
Sanniva Shakya <sannnnnivva@gmail.com>
2025-04-18 20:04:34 +0545

    Added Edit User Profile

---
+++ main/java/com/nestandrest/controller/EditUserProfileController.java
@@ -0,0 +1,22 @@
+package com.nestandrest.controller;
+
+import java.io.IOException;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/edit-user-profile")
+public class EditUserProfileController extends HttpServlet{
+	    private static final long serialVersionUID = 1L;
+
+	    @Override
+	    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        request.getRequestDispatcher("/WEB-INF/pages/user-management/edit-user-profile.jsp").forward(request, response);
+	    }
+}
