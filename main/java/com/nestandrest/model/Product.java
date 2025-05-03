67738e8897e7af2a93b67742011d55af6ef1a259
Bhumika Karki <karkibhumi015@gmail.com>
2025-05-03 09:51:02 +0545

    Updated Files for Add to Cart

--- main/java/com/nestandrest/model/Product.java
+++ main/java/com/nestandrest/model/Product.java
@@ -1,68 +1,72 @@
 package com.nestandrest.model;
 
 public class Product {
+    private int productId;
     private String name;
     private String image;
     private String category;
-    private int price;
+    private double price;
     private int stock;
-    
-    private String Aname, subDescription, fullDescription, Acategory, color, size, Aimage;
-    private double Aprice, discountedPrice;
+
+    private String subDescription;
+    private String fullDescription;
+    private String color;
+    private String size;
+    private double discountedPrice;
     private int quantity;
-    private boolean inStock, published;
+    private boolean inStock;
+    private boolean published;
 
-
+    // Constructors
     public Product(String name, String image, String category, int price) {
         this.name = name;
         this.image = image;
         this.category = category;
         this.price = price;
     }
-    
-    public Product()
-    {
-    	
-    }
+
+    public Product() {}
+
+    // === Getters and Setters ===
+    public int getProductId() { return productId; }
+    public void setProductId(int productId) { this.productId = productId; }
 
     public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
     public String getImage() { return image; }
+    public void setImage(String image) { this.image = image; }
+
     public String getCategory() { return category; }
-    public int getPrice() { return price; }
+    public void setCategory(String category) { this.category = category; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
     public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; }
 
-public void setStock(int stock)
-{
-	this.stock = stock;
+    public String getSubDescription() { return subDescription; }
+    public void setSubDescription(String subDescription) { this.subDescription = subDescription; }
+
+    public String getFullDescription() { return fullDescription; }
+    public void setFullDescription(String fullDescription) { this.fullDescription = fullDescription; }
+
+    public String getColor() { return color; }
+    public void setColor(String color) { this.color = color; }
+
+    public String getSize() { return size; }
+    public void setSize(String size) { this.size = size; }
+
+    public double getDiscountedPrice() { return discountedPrice; }
+    public void setDiscountedPrice(double discountedPrice) { this.discountedPrice = discountedPrice; }
+
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    public boolean isInStock() { return inStock; }
+    public void setInStock(boolean inStock) { this.inStock = inStock; }
+
+    public boolean isPublished() { return published; }
+    public void setPublished(boolean published) { this.published = published; }
 }
-
-
-//=== Setters ===
-public void setName(String Aname) { this.Aname = name; }
-public void setSubDescription(String subDescription) { this.subDescription = subDescription; }
-public void setFullDescription(String fullDescription) { this.fullDescription = fullDescription; }
-public void setCategory(String Acategory) { this.Acategory = category; }
-public void setColor(String color) { this.color = color; }
-public void setSize(String size) { this.size = size; }
-public void setImage(String Aimage) { this.Aimage = image; }
-public void setAPrice(double Aprice) { this.price = price; }
-public void setDiscountedPrice(double discountedPrice) { this.discountedPrice = discountedPrice; }
-public void setQuantity(int quantity) { this.quantity = quantity; }
-public void setInStock(boolean inStock) { this.inStock = inStock; }
-public void setPublished(boolean published) { this.published = published; }
-
-// === Getters (Optional, but useful later) ===
-public String getAName() { return name; }
-public String getSubDescription() { return subDescription; }
-public String getFullDescription() { return fullDescription; }
-public String getACategory() { return category; }
-public String getColor() { return color; }
-public String getSize() { return size; }
-public String getAImage() { return image; }
-public double getAPrice() { return price; }
-public double getDiscountedPrice() { return discountedPrice; }
-public int getQuantity() { return quantity; }
-public boolean isInStock() { return inStock; }
-public boolean isPublished() { return published; }
-}
-
